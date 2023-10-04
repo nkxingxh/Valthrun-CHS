9826e3e7cd180f1834a6394349fcf91dbe8caf8d
NKXingXh <nkxingxh@nkxingxh.top>
2023-10-04 13:38:56 +0800

    to_wide_chars

--- overlay/src/window_tracker.rs
+++ overlay/src/window_tracker.rs
@@ -1,3 +1,23 @@
+extern crate winapi;
+extern crate user32;
+
+fn from_wide_ptr(ptr: *const u16) -> String {
+    use std::ffi::OsString;
+    use std::os::windows::ffi::OsStringExt;
+    unsafe {
+        assert!(!ptr.is_null());
+        let len = (0..std::isize::MAX).position(|i| *ptr.offset(i) == 0).unwrap();
+        let slice = std::slice::from_raw_parts(ptr, len);
+        OsString::from_wide(slice).to_string_lossy().into_owned()
+    }
+}
+
+fn to_wide_chars(s: &str) -> Vec<u16> {
+    use std::ffi::OsStr;
+    use std::os::windows::ffi::OsStrExt;
+    OsStr::new(s).encode_wide().chain(Some(0).into_iter()).collect::<Vec<_>>()
+}
+
 use std::ffi::CString;
 
 use crate::error::{OverlayError, Result};
@@ -25,9 +45,10 @@ pub struct WindowTracker {
 
 impl WindowTracker {
     pub fn new(target: &str) -> Result<Self> {
-        let target = CString::new(target).map_err(OverlayError::WindowInvalidName)?;
-
         log::info!("正在寻找窗口: {:?}", target);
+        //let target = CString::new(target).map_err(OverlayError::WindowInvalidName)?;
+        let target = to_wide_chars(target);
+
         let cs2_hwnd =
             unsafe { FindWindowA(PCSTR::null(), PCSTR::from_raw(target.as_ptr() as *const u8)) };
         if cs2_hwnd.0 == 0 {
