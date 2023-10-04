82975fc6ca37e9a3691116eba08f85dc303adfa9
NKXingXh <nkxingxh@nkxingxh.top>
2023-10-04 12:42:36 +0800

    Update main.rs

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -475,8 +475,11 @@ fn main_overlay() -> anyhow::Result<()> {
     //适配 反恐精英：全球攻势
     let mut overlay = match overlay::init(obfstr!("C2OL"), obfstr!("Counter-Strike 2")) {
         Ok(v) => v,
-        Err(_e) => overlay::init(obfstr!("C2OL"), obfstr!("反恐精英：全球攻势"))
-    }?;
+        Err(_e) => match overlay::init(obfstr!("C2OL"), obfstr!("反恐精英：全球攻势")) {
+            Ok(v) => v,
+            Err(_e) => return _e
+        }
+    };
     if let Some(imgui_settings) = imgui_settings {
         overlay.imgui.load_ini_settings(&imgui_settings);
     }
