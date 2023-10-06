eec32483afc771c653a37409dc97dc576f3d384d
NKXingXh <nkxingxh@nkxingxh.top>
2023-10-06 23:56:30 +0800

    Remove useless dependencies/code

--- overlay/src/window_tracker.rs
+++ overlay/src/window_tracker.rs
@@ -1,17 +1,3 @@
-extern crate winapi;
-extern crate user32;
-
-/* fn from_wide_ptr(ptr: *const u16) -> String {
-    use std::ffi::OsString;
-    use std::os::windows::ffi::OsStringExt;
-    unsafe {
-        assert!(!ptr.is_null());
-        let len = (0..std::isize::MAX).position(|i| *ptr.offset(i) == 0).unwrap();
-        let slice = std::slice::from_raw_parts(ptr, len);
-        OsString::from_wide(slice).to_string_lossy().into_owned()
-    }
-} */
-
 fn to_wide_chars(s: &str) -> Vec<u16> {
     use std::ffi::OsStr;
     use std::os::windows::ffi::OsStrExt;
